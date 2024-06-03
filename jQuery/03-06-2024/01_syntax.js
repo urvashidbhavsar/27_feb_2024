f9ab01e8f262ec37eb9d9c85060e2be81885619a
urvashi <urvashi.d.bhavsar@gmail.com>
2024-06-03 16:10:15 +0530

    3rd changes

---
+++ jQuery/03-06-2024/01_syntax.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // added when any events are appling
+    $("selector").event_method(function(){
+        $("selector").jquery_functions()
+    })
+})
---
+++ jQuery/03-06-2024/02_functions_list.js
@@ -0,0 +1,74 @@
+// effective function/ methods 
+
+// display 
+hide(), show(), toggle()
+
+// fade 
+fadeIn(), fadeOut(), fadeToggle(), fadeTo()
+
+// slide 
+slideUp(), slideDown(), slideToggle()
+
+// animate 
+animate()
+
+// stop
+stop()
+
+callback
+chaining
+
+// for effected with css 
+
+// css
+css()
+
+// css class method 
+addClass(), removeClass(), toggleClass()
+
+// to get a class 
+hasClass()
+
+// effective with structure / DOM manipulation 
+// set and get attribute
+text(), html(), val()
+
+// add element 
+append(), prepend(), before(), after()
+
+// copy element and move from other place 
+appendTo(), prependTo(), insertBefore(), insertAfter()
+
+// copy 
+clone()
+
+// remove element 
+remove(), empty()
+
+// wrapping element 
+wrap(), wrapAll(), wrapInner(), unwrap()
+
+// dimentions 
+height(), width(), innerHeight(), innerWidth(), outerHeight(), outerWidth()
+
+// set element attribute
+attr(), removeAttr()
+
+// set or get property
+prop()
+
+// DOM traversing 
+
+// ancestor 
+parent(), parents(), parentsUntil()
+
+// descendent
+children(), find()
+
+// siblings
+siblings(), next(), nextAll(), nextUntil(), prev(), prevAll(), prevUntil()
+
+// filter 
+filter(), first(), last(), eq(), not() 
+
+// etc ... 
